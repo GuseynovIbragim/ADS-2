4e2fe08030e18da6da59877094800dd6ba1b1458
GuseynovIbragim <78927590+GuseynovIbragim@users.noreply.github.com>
2021-04-04 18:20:04 +0300

    Гусейнов Ибрагим

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,7 +1,6 @@
 // Copyright 2021 NNTU-CS
 
-int countPairs1(int* arr, int len, int value)
-{
+int countPairs1(int* arr, int len, int value) {
 int cnt = 0;
 for (int i = 0; i < len - 1; i++) {
 for (int j = i + 1; j < len; j++) {
@@ -12,8 +11,7 @@ cnt++;
 }
 return cnt;
 }
-int countPairs2(int* arr, int len, int value)
-{
+int countPairs2(int* arr, int len, int value) {
 int cnt = 0, r = len - 1, l = 0, mid;
 while (l < r - 1) {
 mid = (l + r) / 2;
@@ -36,36 +34,29 @@ break;
 }
 return cnt;
 }
-int countPairs3(int* arr, int len, int value)
-{
+int countPairs3(int* arr, int len, int value) {
 int left = 0, right = len - 1, middle, count = 0, i = 0;
 int pseudoValue = value - arr[0];
 if (pseudoValue <= 0)
 return 0;
-for (i = 0; i < len; i++)
-{
+for (i = 0; i < len; i++) {
 pseudoValue = value - arr[i];
 if (pseudoValue <= 0)
 break;
 left = i;
 right = len - 1;
-if (arr[left] <= pseudoValue && pseudoValue <= arr[right])
-{
-while (left <= right)
-{
+if (arr[left] <= pseudoValue && pseudoValue <= arr[right]) {
+while (left <= right) {
 middle = (left + right) / 2;
-if (arr[middle] == pseudoValue)
-{
+if (arr[middle] == pseudoValue) {
 count++;
 int buff = middle + 1;
-while (buff < len && arr[buff] == pseudoValue)
-{
+while (buff < len && arr[buff] == pseudoValue) {
 count++;
 buff++;
 }
 buff = middle - 1;
-while (buff >= 0 && arr[buff] == pseudoValue)
-{
+while (buff >= 0 && arr[buff] == pseudoValue) {
 count++;
 buff--;
 }
