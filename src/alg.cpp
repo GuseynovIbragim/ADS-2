bef2f2c4fc6f2f693fcf23c39ae153f51d78efe1
GuseynovIbragim <78927590+GuseynovIbragim@users.noreply.github.com>
2021-04-04 12:30:48 +0300

    Гусейнов Ибрагим

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,11 +1,9 @@
 // Copyright 2021 NNTU-CS
 
 int countPairs1(int *arr, int len, int value) {
-      int count = 0;
-    for (int i = 0; i < len; i++)
-    {
-        for (int j = i + 1; j < len; j++)
-        {
+    int count = 0;
+    for (int i = 0; i < len; i++){
+        for (int j = i + 1; j < len; j++){
             if (arr[i] != arr[j] && arr[i] + arr[j] == value)
                 count++;
         }
@@ -14,14 +12,11 @@ int countPairs1(int *arr, int len, int value) {
 }
 
 int countPairs2(int *arr, int len, int value) {
-      int count = 0;
+    int count = 0;
     int i = 0, j = len - 1;
-    while (arr[i] < arr[j])
-    {
-        while (arr[i] < arr[j])
-        {
-            if (arr[i] != arr[j])
-            {
+    while (arr[i] < arr[j]){
+        while (arr[i] < arr[j]){
+            if (arr[i] != arr[j]){
                 if (arr[i] + arr[j] < value)
                     break;
                 else if (arr[i] + arr[j] == value)
@@ -36,34 +31,28 @@ int countPairs2(int *arr, int len, int value) {
 }
 
 int countPairs3(int *arr, int len, int value) {
-      int left = 0, right = len - 1, middle, count = 0, i = 0;
+    int left = 0, right = len - 1, middle, count = 0, i = 0;
     int pseudoValue = value - arr[0];
     if (pseudoValue <= 0)
         return 0;
-    for (i = 0; i < len; i++)
-    {
+    for (i = 0; i < len; i++){
         pseudoValue = value - arr[i];
         if (pseudoValue <= 0)
             break;
         left = i;
         right = len - 1;
-        if (arr[left] <= pseudoValue && pseudoValue <= arr[right])
-        {
-            while (left <= right)
-            {
+        if (arr[left] <= pseudoValue && pseudoValue <= arr[right]){
+            while (left <= right){
                 middle = (left + right) / 2;
-                if (arr[middle] == pseudoValue)
-                {
+                if (arr[middle] == pseudoValue){
                     count++;
                     int buff = middle + 1;
-                    while (buff < len && arr[buff] == pseudoValue)
-                    {
+                    while (buff < len && arr[buff] == pseudoValue){
                         count++;
                         buff++;
                     }
                     buff = middle - 1;
-                    while (buff >= 0 && arr[buff] == pseudoValue)
-                    {
+                    while (buff >= 0 && arr[buff] == pseudoValue){
                         count++;
                         buff--;
                     }
